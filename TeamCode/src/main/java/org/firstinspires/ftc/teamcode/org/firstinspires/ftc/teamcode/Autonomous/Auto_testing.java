f5f55626b7864dce3cc4fd4c95adce9bf252c741
JordanPag <jpags2938@gmail.com>
2019-10-03 08:47:45 -0400

    Make a basic file to test autonomous function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/org/firstinspires/ftc/teamcode/Autonomous/Auto_testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/org/firstinspires/ftc/teamcode/Autonomous/Auto_testing.java
@@ -1,4 +1,159 @@
 package org.firstinspires.ftc.teamcode.org.firstinspires.ftc.teamcode.Autonomous;
 
-public class Auto_testing {
+/**
+ * Created by Jordan Paglione on 10/3/19
+ */
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Locale;
+
+@Autonomous(name = "BLUE 2", group = "Sensor")
+public class Auto_testing extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FrontLeft;
+    private DcMotor FrontRight;
+    private DcMotor BackLeft;
+    private DcMotor BackRight;
+    private DcMotor IntakeLeft;
+    private DcMotor IntakeRight;
+    //private Servo Servo1;
+
+    @Override
+    public void runOpMode() {
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        FrontLeft  = hardwareMap.get(DcMotor.class, "FrontLeft");
+        BackLeft  = hardwareMap.get(DcMotor.class, "BackLeft");
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        BackRight = hardwareMap.get(DcMotor.class, "BackRight");
+        //IntakeLeft = hardwareMap.get(DcMotor.class, "IntakeLeft");
+        //IntakeRight = hardwareMap.get(DcMotor.class, "IntakeRight");
+        //Servo1 = hardwareMap.get(Servo.class, "Servo1");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
+        //IntakeLeft.setDirection(DcMotor.Direction.FORWARD);
+        //IntakeRight.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            // Start button is pressed
+
+            moveForward(50,1000);
+            moveBackward(50,1000);
+            strafeLeft(50,1000);
+            strafeRight(50,1000);
+            turnLeft(50,1000);
+            turnRight(50,1000);
+
+            // End of auto
+            break;
+        }
+    }
+
+    // Basic functions
+
+    public void delay(int time) {
+        double startTime = runtime.milliseconds();
+        while (runtime.milliseconds() - startTime < time) {
+        }
+    }
+
+    public void moveForward(double power, int time) {
+        FrontLeft.setPower(power);
+        FrontRight.setPower(power);
+        BackLeft.setPower(power);
+        BackRight.setPower(power);
+        delay(time);
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void moveBackward(double power, int time) {
+        FrontLeft.setPower(-power);
+        FrontRight.setPower(-power);
+        BackLeft.setPower(-power);
+        BackRight.setPower(-power);
+        delay(time);
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void strafeRight(double power, int time){
+        FrontLeft.setPower(power);
+        FrontRight.setPower(-power);
+        BackLeft.setPower(-power);
+        BackRight.setPower(power);
+        delay(time);
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void strafeLeft(double power, int time){
+        FrontLeft.setPower(-power);
+        FrontRight.setPower(power);
+        BackLeft.setPower(power);
+        BackRight.setPower(-power);
+        delay(time);
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void turnRight(double power, int time){
+        FrontLeft.setPower(power);
+        FrontRight.setPower(-power);
+        BackLeft.setPower(power);
+        BackRight.setPower(-power);
+        delay(time);
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void turnLeft(double power, int time){
+        FrontLeft.setPower(-power);
+        FrontRight.setPower(power);
+        BackLeft.setPower(-power);
+        BackRight.setPower(power);
+        delay(time);
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
 }
