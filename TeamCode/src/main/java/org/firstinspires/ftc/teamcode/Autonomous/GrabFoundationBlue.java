3fecdd693a0cc475f10831456e5e849e5d918cae
JordanPag <jpags2938@gmail.com>
2019-10-10 15:44:24 -0400

    Changed file structure to make more sense

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/GrabFoundationBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/GrabFoundationBlue.java
@@ -1,7 +1,7 @@
-package org.firstinspires.ftc.teamcode.org.firstinspires.ftc.teamcode.Autonomous;
+package org.firstinspires.ftc.teamcode.Autonomous;
 
 /**
- * Created by Jordan Paglione on 10/3/19
+ * Created by Jordan Paglione on 10/7/19
  */
 
 import android.app.Activity;
@@ -13,7 +13,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -27,8 +26,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.util.Locale;
 
-@Autonomous(name = "Auto testing", group = "Sensor")
-public class Auto_testing extends LinearOpMode {
+@Autonomous(name = "Grab_Foundation_Blue", group = "Sensor")
+public class GrabFoundationBlue extends LinearOpMode{
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -68,20 +67,18 @@ public class Auto_testing extends LinearOpMode {
         waitForStart();
         while (opModeIsActive()) {
             // Start button is pressed
+            FoundationServo.setPosition(0);
+            strafeRightWithEncoders(0.5, 2300);
+            FoundationServo.setPosition(0.5);
+            delay(500);
+            strafeLeftWithEncoders(0.5,2800);
+            FoundationServo.setPosition(0);
+            moveBackwardWithEncoders(0.5, 2000);
 
-            moveForward(0.5,1000);
-            moveBackward(0.5,1000);
-            strafeLeft(0.5,1000);
-            strafeRight(0.5,1000);
-            turnLeft(0.5,1000);
-            turnRight(0.5,1000);
-
-            // End of auto
             break;
+            // End of auto
         }
     }
-
-    // Basic functions
 
     public void delay(int time) {
         double startTime = runtime.milliseconds();
@@ -89,72 +86,51 @@ public class Auto_testing extends LinearOpMode {
         }
     }
 
-    public void moveForward(double power, int time) {
-        FrontLeft.setPower(power);
-        FrontRight.setPower(power);
-        BackLeft.setPower(power);
-        BackRight.setPower(power);
-        delay(time);
-        FrontLeft.setPower(0);
-        FrontRight.setPower(0);
-        BackLeft.setPower(0);
-        BackRight.setPower(0);
-    }
-
-    public void moveBackward(double power, int time) {
-        FrontLeft.setPower(-power);
-        FrontRight.setPower(-power);
-        BackLeft.setPower(-power);
-        BackRight.setPower(-power);
-        delay(time);
-        FrontLeft.setPower(0);
-        FrontRight.setPower(0);
-        BackLeft.setPower(0);
-        BackRight.setPower(0);
-    }
-
-    public void strafeRight(double power, int time){
+    public void strafeRightWithEncoders(double power, int count){
+        int start = FrontLeft.getCurrentPosition();
         FrontLeft.setPower(power);
         FrontRight.setPower(-power);
         BackLeft.setPower(-power);
         BackRight.setPower(power);
-        delay(time);
+        while(FrontLeft.getCurrentPosition() < start + count) {
+            telemetry.addData("Left motor position", FrontLeft.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
         FrontLeft.setPower(0);
         FrontRight.setPower(0);
         BackLeft.setPower(0);
         BackRight.setPower(0);
     }
 
-    public void strafeLeft(double power, int time){
+    public void strafeLeftWithEncoders(double power, int count){
+        int start = FrontRight.getCurrentPosition();
         FrontLeft.setPower(-power);
         FrontRight.setPower(power);
         BackLeft.setPower(power);
         BackRight.setPower(-power);
-        delay(time);
+        while(FrontRight.getCurrentPosition() < start + count) {
+            telemetry.addData("Right motor position", FrontRight.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
         FrontLeft.setPower(0);
         FrontRight.setPower(0);
         BackLeft.setPower(0);
         BackRight.setPower(0);
     }
 
-    public void turnRight(double power, int time){
-        FrontLeft.setPower(power);
+    public void moveBackwardWithEncoders(double power, int count){
+        int start = FrontLeft.getCurrentPosition();
+        FrontLeft.setPower(-power);
         FrontRight.setPower(-power);
-        BackLeft.setPower(power);
-        BackRight.setPower(-power);
-        delay(time);
-        FrontLeft.setPower(0);
-        FrontRight.setPower(0);
-        BackLeft.setPower(0);
-        BackRight.setPower(0);
-    }
-
-    public void turnLeft(double power, int time){
-        FrontLeft.setPower(-power);
-        FrontRight.setPower(power);
         BackLeft.setPower(-power);
-        BackRight.setPower(power);
-        delay(time);
+        BackRight.setPower(-power);
+        while(FrontLeft.getCurrentPosition() > start - count) {
+            telemetry.addData("Left motor position", FrontLeft.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
         FrontLeft.setPower(0);
         FrontRight.setPower(0);
         BackLeft.setPower(0);
