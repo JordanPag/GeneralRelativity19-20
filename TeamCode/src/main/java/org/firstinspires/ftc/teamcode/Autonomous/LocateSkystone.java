9879fc5a699941c683574f7d640258b02be6f19b
JordanPag <jpags2938@gmail.com>
2019-12-27 17:59:29 -0500

    Made a file that uses a color sensor to find a skystone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/LocateSkystone.java
@@ -0,0 +1,272 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+/**
+ * Created by Jordan Paglione on 10/7/19
+ */
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Locale;
+
+@Autonomous(name = "Locate Skystone", group = "Sensor")
+public class LocateSkystone extends LinearOpMode{
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FrontLeft;
+    private DcMotor FrontRight;
+    private DcMotor BackLeft;
+    private DcMotor BackRight;
+    private DcMotor IntakeLeft;
+    private DcMotor IntakeRight;
+    private DcMotor Treadmill;
+    private Servo FoundationServo;
+    ColorSensor sensorColor;
+    DistanceSensor sensorDistance;
+
+    @Override
+    public void runOpMode() {
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        BackRight = hardwareMap.get(DcMotor.class, "BackRight");
+        IntakeLeft = hardwareMap.get(DcMotor.class, "IntakeLeft");
+        IntakeRight = hardwareMap.get(DcMotor.class, "IntakeRight");
+        Treadmill = hardwareMap.get(DcMotor.class, "Treadmill");
+        FoundationServo = hardwareMap.get(Servo.class, "FoundationServo");
+
+        sensorColor = hardwareMap.get(ColorSensor.class, "ColorSensor");
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "ColorSensor");
+        float hsvValues[] = {0F, 0F, 0F};
+        final float values[] = hsvValues;
+        final double SCALE_FACTOR = 255;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
+        IntakeLeft.setDirection(DcMotor.Direction.REVERSE);
+        IntakeRight.setDirection(DcMotor.Direction.FORWARD);
+        Treadmill.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            // Start button is pressed
+
+            //Go to the block
+            FoundationServo.setPosition(0);
+            strafeRightWithEncoders(0.5, 1750);
+            turnLeftWithEncoders(0.3, 1);
+            moveForwardWithEncoders(0.5,100);
+            delay(300);
+
+            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+                    (int) (sensorColor.green() * SCALE_FACTOR),
+                    (int) (sensorColor.blue() * SCALE_FACTOR),
+                    hsvValues);
+
+            telemetry.addData("Distance (cm)",
+                    String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
+            telemetry.addData("Alpha", sensorColor.alpha());
+            telemetry.addData("Red  ", sensorColor.red());
+            telemetry.addData("Green", sensorColor.green());
+            telemetry.addData("Blue ", sensorColor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+            boolean isSkystone = false;
+            int position = 0;
+            if(hsvValues[0] >= 60.0) {
+                isSkystone = true;
+                position = 2;
+            }
+            telemetry.addData("Skystone", isSkystone);
+            telemetry.update();
+
+            if(!isSkystone) {
+                moveForwardWithEncoders(0.5,250);
+                delay(300);
+
+                Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+                        (int) (sensorColor.green() * SCALE_FACTOR),
+                        (int) (sensorColor.blue() * SCALE_FACTOR),
+                        hsvValues);
+
+                telemetry.addData("Distance (cm)",
+                        String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
+                telemetry.addData("Alpha", sensorColor.alpha());
+                telemetry.addData("Red  ", sensorColor.red());
+                telemetry.addData("Green", sensorColor.green());
+                telemetry.addData("Blue ", sensorColor.blue());
+                telemetry.addData("Hue", hsvValues[0]);
+                isSkystone = false;
+                if(hsvValues[0] >= 60.0) {
+                    isSkystone = true;
+                    position = 1;
+                } else {
+                    position = 3;
+                }
+            }
+
+            telemetry.addData("Skystone position", position);
+            telemetry.update();
+            delay(1500);
+
+
+            if (position == 1) {
+                moveBackwardWithEncoders(0.5, 1050);
+            } else if (position == 2) {
+                moveBackwardWithEncoders(0.5, 1000);
+            } else {
+                moveBackwardWithEncoders(0.5,550);
+            }
+            strafeRightWithEncoders(0.5, 700);
+            IntakeLeft.setPower(0.9);
+            IntakeRight.setPower(0.9);
+            Treadmill.setPower(1.0);
+            moveForwardWithEncoders(0.5, 400);
+            delay(1000);
+            IntakeLeft.setPower(0);
+            IntakeRight.setPower(0);
+            delay(300);
+            Treadmill.setPower(0);
+
+            // End of auto
+            break;
+        }
+    }
+
+    public void delay(int time) {
+        double startTime = runtime.milliseconds();
+        while (runtime.milliseconds() - startTime < time) {
+        }
+    }
+
+    public void strafeRightWithEncoders(double power, int count){
+        int start = FrontLeft.getCurrentPosition();
+        FrontLeft.setPower(power);
+        FrontRight.setPower(-power);
+        BackLeft.setPower(-power);
+        BackRight.setPower(power);
+        while(FrontLeft.getCurrentPosition() < start + count) {
+            telemetry.addData("Left motor position", FrontLeft.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void strafeLeftWithEncoders(double power, int count){
+        int start = FrontRight.getCurrentPosition();
+        FrontLeft.setPower(-power);
+        FrontRight.setPower(power);
+        BackLeft.setPower(power);
+        BackRight.setPower(-power);
+        while(FrontRight.getCurrentPosition() < start + count) {
+            telemetry.addData("Right motor position", FrontRight.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void moveBackwardWithEncoders(double power, int count){
+        int start = FrontLeft.getCurrentPosition();
+        FrontLeft.setPower(-power);
+        FrontRight.setPower(-power);
+        BackLeft.setPower(-power);
+        BackRight.setPower(-power);
+        while(FrontLeft.getCurrentPosition() > start - count) {
+            telemetry.addData("Left motor position", FrontLeft.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void moveForwardWithEncoders(double power, int count){
+        int start = FrontLeft.getCurrentPosition();
+        FrontLeft.setPower(power);
+        FrontRight.setPower(power);
+        BackLeft.setPower(power);
+        BackRight.setPower(power);
+        while(FrontLeft.getCurrentPosition() < start + count) {
+            telemetry.addData("Left motor position", FrontLeft.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void turnLeftWithEncoders(double power, int count){
+        int start = FrontRight.getCurrentPosition();
+        FrontLeft.setPower(-power);
+        FrontRight.setPower(power);
+        BackLeft.setPower(-power);
+        BackRight.setPower(power);
+        while(FrontRight.getCurrentPosition() < start + count) {
+            telemetry.addData("Right motor position", FrontRight.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    public void turnRightWithEncoders(double power, int count){
+        int start = FrontLeft.getCurrentPosition();
+        FrontLeft.setPower(power);
+        FrontRight.setPower(-power);
+        BackLeft.setPower(power);
+        BackRight.setPower(-power);
+        while(FrontLeft.getCurrentPosition() < start + count) {
+            telemetry.addData("Left motor position", FrontLeft.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+}
